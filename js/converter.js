c7ca4a98720aadb140f4270e5cfa3339962c4ff0
jmyshadow <jmachads@gmail.com>
2020-07-05 16:12:28 -0400

    label fix

--- js/converter.js
+++ js/converter.js
@@ -30,7 +30,7 @@ let hiraganaButton = document.getElementById('hiragana-button');
 let katakanaButton = document.getElementById('katakana-button');
 
 
-function convertText(){
+function convertText() {
     //keep output the same if already converted   
     let text = textBox.value.split('');
     let nonConv = '';
@@ -38,51 +38,51 @@ function convertText(){
     let hiragana = '';
     let katakana = '';
     let n = false;
-    
+
     text.forEach(kana => {
         nonConv = nonConv + kana;
 
         // check if need to convert to NX kana
-        if(n){
-            if(['na','ni','nu','no','nn','ne','nya','nyo','nyu'].includes(nonConv)){
-                hiragana = hiragana.substring(0,hiragana.length-1);
-                katakana = katakana.substring(0,katakana.length-1);
+        if (n) {
+            if (['na', 'ni', 'nu', 'no', 'nn', 'ne', 'nya', 'nyo', 'nyu'].includes(nonConv)) {
+                hiragana = hiragana.substring(0, hiragana.length - 1);
+                katakana = katakana.substring(0, katakana.length - 1);
                 n = false;
             }
         }
 
         // check nonConv == 'n' to maybe convert to NX kana
-        if(nonConv == 'n'){
+        if (nonConv == 'n') {
             n = true;
         }
 
         // check for double consonants
-       if (nonConv[0]==[nonConv[1]]){
-           if(nonConv !='nn'){
-               hiragana += hLookup['xtsu'];
-               katakana += kLookup['xtsu'];
-            } 
+        if (nonConv[0] == [nonConv[1]]) {
+            if (nonConv != 'nn') {
+                hiragana += hLookup['xtsu'];
+                katakana += kLookup['xtsu'];
+            }
             nonConv = nonConv[0];
-        } 
+        }
 
 
 
-        if(hLookup[nonConv]){
+        if (hLookup[nonConv]) {
             hiragana += hLookup[nonConv];
             katakana += kLookup[nonConv];
             conv = conv + nonConv;
 
             //leave value as 'n' to check for NX kana
-            if(nonConv != 'n')
+            if (nonConv != 'n')
                 nonConv = '';
-        } 
-        
-        if(nonConv.length > 3){
+        }
+
+        if (nonConv.length > 3) {
             document.getElementById('error').classList.remove('hidden');
         } else {
             document.getElementById('error').classList.add('hidden');
         }
-        
+
     })
     hiraganaBox.value = hiragana;
     katakanaBox.value = katakana;
@@ -90,279 +90,295 @@ function convertText(){
     console.log(nonConv + ' hasn\'t been converted');
 }
 
-function hToggle(){
+function hToggle() {
     document.getElementById('hiragana-div').classList.toggle('hidden');
     katakanaBox.classList.toggle('right');
     this.classList.toggle('deselect');
-    
+    document.getElementById('hiragana-label').classList.toggle('hidden');
+
+    if (document.getElementById('hiragana-label').classList.contains('hidden')) {
+        document.getElementById("kSpan").style.transform = "translateX(-20%)";
+    }
+    else {
+        document.getElementById("kSpan").style.transform = "";
+    }
+
 }
 
-function kToggle(){
+function kToggle() {
     document.getElementById('katakana-div').classList.toggle('hidden');
     hiraganaBox.classList.toggle('left');
     this.classList.toggle('deselect');
+
+    document.getElementById('katakana-label').classList.toggle('hidden');
+    if (document.getElementById('katakana-label').classList.contains('hidden')) {
+        document.getElementById('hSpan').style.transform = "translateX(-20%)";
+    }
+    else {
+        document.getElementById('hSpan').style.transform = "";
+    }
 }
 
 //add code to find double consonants
 let kLookup = {
-    "a":"ア",
-    "i":"イ",
-    "u":"ウ",
-    "e":"エ",
-    "o":"オ",
-    "ka":"カ",
-    "ki":"キ",
-    "ku":"ク",
-    "ke":"ケ",
-    "ko":"コ",
-    "sa":"サ",
-    "shi":"シ",
-    "su":"ス",
-    "se":"セ",
-    "so":"ソ",
-    "ta":"タ",
-    "chi":"チ",
-    "tsu":"ツ",
-    "te":"テ",
-    "to":"ト",
-    "na":"ナ",
-    "ni":"ニ",
-    "nu":"ヌ",
-    "ne":"ネ",
-    "no":"ノ",
-    "ha":"ハ",
-    "hi":"ヒ",
-    "fu":"フ",
-    "he":"ヘ",
-    "ho":"ホ",
-    "ma":"マ",
-    "mi":"ミ",
-    "mu":"ム",
-    "me":"メ",
-    "mo":"モ",
-    "ya":"ヤ",
-    "yu":"ユ",
-    "yo":"ヨ",
-    "ra":"ラ",
-    "ri":"リ",
-    "ru":"ル",
-    "re":"レ",
-    "ro":"ロ",
-    "wa":"ワ",
-    "wo":"ヲ",
-    "n":"ン",
-    "nn":"ン",
+    "a": "ア",
+    "i": "イ",
+    "u": "ウ",
+    "e": "エ",
+    "o": "オ",
+    "ka": "カ",
+    "ki": "キ",
+    "ku": "ク",
+    "ke": "ケ",
+    "ko": "コ",
+    "sa": "サ",
+    "shi": "シ",
+    "su": "ス",
+    "se": "セ",
+    "so": "ソ",
+    "ta": "タ",
+    "chi": "チ",
+    "tsu": "ツ",
+    "te": "テ",
+    "to": "ト",
+    "na": "ナ",
+    "ni": "ニ",
+    "nu": "ヌ",
+    "ne": "ネ",
+    "no": "ノ",
+    "ha": "ハ",
+    "hi": "ヒ",
+    "fu": "フ",
+    "he": "ヘ",
+    "ho": "ホ",
+    "ma": "マ",
+    "mi": "ミ",
+    "mu": "ム",
+    "me": "メ",
+    "mo": "モ",
+    "ya": "ヤ",
+    "yu": "ユ",
+    "yo": "ヨ",
+    "ra": "ラ",
+    "ri": "リ",
+    "ru": "ル",
+    "re": "レ",
+    "ro": "ロ",
+    "wa": "ワ",
+    "wo": "ヲ",
+    "n": "ン",
+    "nn": "ン",
     //
-    "ga":"ガ",
-    "gi":"ギ",
-    "gu":"グ",
-    "ge":"ゲ",
-    "go":"ゴ",
-    "za":"ザ",
-    "zi":"ジ",
-    "zu":"ズ",
-    "ze":"ゼ",
-    "zo":"ゾ",
-    "da":"ダ",
-    "di":"ヂ",
-    "du":"ヅ",
-    "de":"デ",
-    "do":"ド",
-    "ba":"バ",
-    "bi":"ビ",
-    "bu":"ブ",
-    "be":"ベ",
-    "bo":"ボ",
-    "pa":"パ",
-    "pi":"ピ",
-    "pu":"プ",
-    "pe":"ペ",
-    "po":"ポ",
+    "ga": "ガ",
+    "gi": "ギ",
+    "gu": "グ",
+    "ge": "ゲ",
+    "go": "ゴ",
+    "za": "ザ",
+    "zi": "ジ",
+    "zu": "ズ",
+    "ze": "ゼ",
+    "zo": "ゾ",
+    "da": "ダ",
+    "di": "ヂ",
+    "du": "ヅ",
+    "de": "デ",
+    "do": "ド",
+    "ba": "バ",
+    "bi": "ビ",
+    "bu": "ブ",
+    "be": "ベ",
+    "bo": "ボ",
+    "pa": "パ",
+    "pi": "ピ",
+    "pu": "プ",
+    "pe": "ペ",
+    "po": "ポ",
     // Diagraphs
-    "kya":"キャ",
-    "kyu":"キュ",
-    "kyo":"キョ",
-    "sha":"シャ",
-    "shu":"シュ",
-    "sho":"ショ",
-    "cha":"チャ",
-    "chu":"チュ",
-    "cho":"チョ",
-    "nya":"ニャ",
-    "nyu":"ニュ",
-    "nyo":"ニョ",
-    "hya":"ヒャ",
-    "hyu":"ヒュ",
-    "hyo":"ヒョ",
-    "mya":"ミャ",
-    "myu":"ミュ",
-    "myo":"ミョ",
-    "rya":"リャ",
-    "ryu":"リュ",
-    "ryo":"リョ",
+    "kya": "キャ",
+    "kyu": "キュ",
+    "kyo": "キョ",
+    "sha": "シャ",
+    "shu": "シュ",
+    "sho": "ショ",
+    "cha": "チャ",
+    "chu": "チュ",
+    "cho": "チョ",
+    "nya": "ニャ",
+    "nyu": "ニュ",
+    "nyo": "ニョ",
+    "hya": "ヒャ",
+    "hyu": "ヒュ",
+    "hyo": "ヒョ",
+    "mya": "ミャ",
+    "myu": "ミュ",
+    "myo": "ミョ",
+    "rya": "リャ",
+    "ryu": "リュ",
+    "ryo": "リョ",
     // Diagraphs with Diacritics
-    "gya":"ギャ",
-    "gyu":"ギュ",
-    "gyo":"ギョ",
-    "ja":"ジャ",
-    "ju":"ジュ",
-    "jo":"ジョ",
-    "bya":"ビャ",
-    "byu":"ビュ",
-    "byo":"ビョ",
-    "pya":"ピャ",
-    "pyu":"ピュ",
-    "pyo":"ピョ",
+    "gya": "ギャ",
+    "gyu": "ギュ",
+    "gyo": "ギョ",
+    "ja": "ジャ",
+    "ju": "ジュ",
+    "jo": "ジョ",
+    "bya": "ビャ",
+    "byu": "ビュ",
+    "byo": "ビョ",
+    "pya": "ピャ",
+    "pyu": "ピュ",
+    "pyo": "ピョ",
     // small kana
-    "xya":"ャ",
-    "xyu":"ュ",
-    "xyo":"ョ",
-    "xa":"ァ",
-    "xe":"ェ",
-    "xi":"ィ",
-    "xo":"ォ",
-    "xu":"ゥ",
-    "xtsu":"ッ",
-    "xka":"ヵ",
-    "xke":"ヶ",
+    "xya": "ャ",
+    "xyu": "ュ",
+    "xyo": "ョ",
+    "xa": "ァ",
+    "xe": "ェ",
+    "xi": "ィ",
+    "xo": "ォ",
+    "xu": "ゥ",
+    "xtsu": "ッ",
+    "xka": "ヵ",
+    "xke": "ヶ",
     //
-    ".":"。",
-    "?":"？",
-    "!":"！",
-    "-":"ー",
-    ",":"、"
+    ".": "。",
+    "?": "？",
+    "!": "！",
+    "-": "ー",
+    ",": "、"
 };
 
 let hLookup = {
-    "a":"あ",
-    "i":"い",
-    "u":"う",
-    "e":"え",
-    "o":"お",
-    "ka":"か",
-    "ki":"き",
-    "ku":"く",
-    "ke":"け",
-    "ko":"こ",
-    "sa":"さ",
-    "shi":"し",
-    "su":"す",
-    "se":"せ",
-    "so":"そ",
-    "ta":"た",
-    "chi":"ち",
-    "tsu":"つ",
-    "te":"て",
-    "to":"と",
-    "na":"な",
-    "ni":"に",
-    "nu":"ぬ",
-    "ne":"ね",
-    "no":"の",
-    "ha":"は",
-    "hi":"ひ",
-    "fu":"ふ",
-    "he":"へ",
-    "ho":"ほ",
-    "ma":"ま",
-    "mi":"み",
-    "mu":"む",
-    "me":"め",
-    "mo":"も",
-    "ya":"や",
-    "yu":"ゆ",
-    "yo":"よ",
-    "ra":"ら",
-    "ri":"り",
-    "ru":"る",
-    "re":"れ",
-    "ro":"ろ",
-    "wa":"わ",
-    "wo":"を",
-    "n":"ん",
-    "nn":"ん",
+    "a": "あ",
+    "i": "い",
+    "u": "う",
+    "e": "え",
+    "o": "お",
+    "ka": "か",
+    "ki": "き",
+    "ku": "く",
+    "ke": "け",
+    "ko": "こ",
+    "sa": "さ",
+    "shi": "し",
+    "su": "す",
+    "se": "せ",
+    "so": "そ",
+    "ta": "た",
+    "chi": "ち",
+    "tsu": "つ",
+    "te": "て",
+    "to": "と",
+    "na": "な",
+    "ni": "に",
+    "nu": "ぬ",
+    "ne": "ね",
+    "no": "の",
+    "ha": "は",
+    "hi": "ひ",
+    "fu": "ふ",
+    "he": "へ",
+    "ho": "ほ",
+    "ma": "ま",
+    "mi": "み",
+    "mu": "む",
+    "me": "め",
+    "mo": "も",
+    "ya": "や",
+    "yu": "ゆ",
+    "yo": "よ",
+    "ra": "ら",
+    "ri": "り",
+    "ru": "る",
+    "re": "れ",
+    "ro": "ろ",
+    "wa": "わ",
+    "wo": "を",
+    "n": "ん",
+    "nn": "ん",
     //
-    "ga":"が",
-    "gi":"ぎ",
-    "gu":"ぐ",
-    "ge":"げ",
-    "go":"ご",
-    "za":"ざ",
-    "zi":"じ",
-    "zu":"ず",
-    "ze":"ぜ",
-    "zo":"ぞ",
-    "da":"だ",
-    "di":"ぢ",
-    "du":"づ",
-    "de":"で",
-    "do":"ど",
-    "ba":"ば",
-    "bi":"び",
-    "bu":"ぶ",
-    "be":"べ",
-    "bo":"ぼ",
-    "pa":"ぱ",
-    "pi":"ぴ",
-    "pu":"ぷ",
-    "pe":"ぺ",
-    "po":"ぽ",
+    "ga": "が",
+    "gi": "ぎ",
+    "gu": "ぐ",
+    "ge": "げ",
+    "go": "ご",
+    "za": "ざ",
+    "zi": "じ",
+    "zu": "ず",
+    "ze": "ぜ",
+    "zo": "ぞ",
+    "da": "だ",
+    "di": "ぢ",
+    "du": "づ",
+    "de": "で",
+    "do": "ど",
+    "ba": "ば",
+    "bi": "び",
+    "bu": "ぶ",
+    "be": "べ",
+    "bo": "ぼ",
+    "pa": "ぱ",
+    "pi": "ぴ",
+    "pu": "ぷ",
+    "pe": "ぺ",
+    "po": "ぽ",
     // Diagraphs
-    "kya":"きゃ",
-    "kyu":"きゅ",
-    "kyo":"きょ",
-    "sha":"しゃ",
-    "shu":"しゅ",
-    "sho":"しょ",
-    "cha":"ちゃ",
-    "chu":"ちゅ",
-    "cho":"ちょ",
-    "nya":"にゃ",
-    "nyu":"にゅ",
-    "nyo":"にょ",
-    "hya":"ひゃ",
-    "hyu":"ひゅ",
-    "hyo":"ひょ",
-    "mya":"みゃ",
-    "myu":"みゅ",
-    "myo":"みょ",
-    "rya":"りゃ",
-    "ryu":"りゅ",
-    "ryo":"りょ",
+    "kya": "きゃ",
+    "kyu": "きゅ",
+    "kyo": "きょ",
+    "sha": "しゃ",
+    "shu": "しゅ",
+    "sho": "しょ",
+    "cha": "ちゃ",
+    "chu": "ちゅ",
+    "cho": "ちょ",
+    "nya": "にゃ",
+    "nyu": "にゅ",
+    "nyo": "にょ",
+    "hya": "ひゃ",
+    "hyu": "ひゅ",
+    "hyo": "ひょ",
+    "mya": "みゃ",
+    "myu": "みゅ",
+    "myo": "みょ",
+    "rya": "りゃ",
+    "ryu": "りゅ",
+    "ryo": "りょ",
     // Diagraphs with Diacritics
-    "gya":"ぎゃ",
-    "gyu":"ぎゅ",
-    "gyo":"ぎょ",
-    "ja":"じゃ",
-    "ju":"じゅ",
-    "jo":"じょ",
-    "bya":"びゃ",
-    "byu":"びゅ",
-    "byo":"びょ",
-    "pya":"ぴゃ",
-    "pyu":"ぴゅ",
-    "pyo":"ぴょ",
+    "gya": "ぎゃ",
+    "gyu": "ぎゅ",
+    "gyo": "ぎょ",
+    "ja": "じゃ",
+    "ju": "じゅ",
+    "jo": "じょ",
+    "bya": "びゃ",
+    "byu": "びゅ",
+    "byo": "びょ",
+    "pya": "ぴゃ",
+    "pyu": "ぴゅ",
+    "pyo": "ぴょ",
     // small kana
-    "xya":"ゃ",
-    "xyu":"ゅ",
-    "xyo":"ょ",
-    "xa":"ぁ",
-    "xe":"ぇ",
-    "xi":"ぃ",
-    "xo":"ぉ",
-    "xu":"ぅ",
-    "xtsu":"っ",
-    "xka":"ゕ",
-    "xke":"ゖ",
+    "xya": "ゃ",
+    "xyu": "ゅ",
+    "xyo": "ょ",
+    "xa": "ぁ",
+    "xe": "ぇ",
+    "xi": "ぃ",
+    "xo": "ぉ",
+    "xu": "ぅ",
+    "xtsu": "っ",
+    "xka": "ゕ",
+    "xke": "ゖ",
     //
-    ".":"。",
-    "?":"？",
-    "!":"！",
-    "-":"ー",
-    ",":"、"
+    ".": "。",
+    "?": "？",
+    "!": "！",
+    "-": "ー",
+    ",": "、"
 };
 
 
-textBox.addEventListener('input',convertText);
-hiraganaButton.addEventListener('click',hToggle);
-katakanaButton.addEventListener('click',kToggle);
+textBox.addEventListener('input', convertText);
+hiraganaButton.addEventListener('click', hToggle);
+katakanaButton.addEventListener('click', kToggle);
